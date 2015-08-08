97caaaa7fc74b80ea77f1abd25506f9e2388af5e
AnaRankovic <anarankovic11@gmail.com>
2015-08-08 17:14:54 +0200

    Drugi commit.

--- mywebapp/project.clj
+++ mywebapp/project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            }
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
-                 [http-kit "2.1.16"]])
+                 [http-kit "2.1.16"]
+                 ])
--- mywebapp/src/mywebapp/core.clj
+++ mywebapp/src/mywebapp/core.clj
@@ -2,8 +2,14 @@
   (:require [compojure.core :refer :all]
             [org.httpkit.server :refer [run-server]])) ; httpkit is a server
 
-(defroutes myapp
-  (GET "/" [] "Cao svete!"))
+(defroutes mywebapp
+  (GET "/" [] "Show something")
+  (POST "/" [] "Create something")
+  (PUT "/" [] "Replace something")
+  (PATCH "/" [] "Modify Something")
+  (DELETE "/" [] "Annihilate something")
+  (OPTIONS "/" [] "Appease something")
+  (HEAD "/" [] "Preview something"))
 
 (defn -main []
-  (run-server myapp {:port 5000}))
+  (run-server mywebapp {:port 5000}))
